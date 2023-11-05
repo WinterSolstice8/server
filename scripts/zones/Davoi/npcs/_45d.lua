53af000461cf9908d3dbc68e40619eee37988f4f
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2023-11-04 21:54:23 -0600

    [lua] Add Wall of Banishing open/close animation

--- scripts/zones/Davoi/npcs/_45d.lua
+++ scripts/zones/Davoi/npcs/_45d.lua
@@ -25,7 +25,12 @@ end
 entity.onEventFinish = function(player, csid, option, npc)
     if csid == 42 and option == 0 then
         player:messageSpecial(ID.text.POWER_OF_THE_ORB_ALLOW_PASS)
-        npc:openDoor(16)
+
+        -- Animation for the "door" being open, npc:openDoor sends a different animation packet and doesn't work.
+        npc:setAnimation(8)
+        npc:timer(1000 * 20, function(npcArg)
+            npcArg:setAnimation(9)
+        end)
     end
 end
 
